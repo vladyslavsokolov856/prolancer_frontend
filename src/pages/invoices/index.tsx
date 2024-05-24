24aaa86bbabfec6b4425887e47106e30c386fd55
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-24 23:37:58 +0800

    change initial invoice display

--- src/pages/invoices/index.tsx
+++ src/pages/invoices/index.tsx
@@ -5,83 +5,81 @@ import Alert from '@mui/material/Alert'
 import Chip from '@mui/material/Chip'
 import { Link } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
-import EditIcon from '@mui/icons-material/Edit'
-import ProTable, { ColumnType, RecordType } from '@/components/ProTable'
+import { useUsers, useDeleteUser } from '@/hooks/useUsers'
 import { Link as RouterLink } from 'react-router-dom'
+import { useMemo, useState } from 'react'
+import ProTable, { ColumnType } from '@/components/ProTable'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import ConfirmDialog from '@/components/ConfirmDialog'
+import User from '@/types/users'
 
-const mockData: RecordType[] = [
-  {
-    id: 1,
-    name: 'asdf',
-    address: 'Hello',
-    telephone: 'telephone',
-    customer_type: 'person',
-    email: 'hello@computer.com',
-  },
-  {
-    id: 2,
-    name: 'asdf1',
-    address: 'Hello1',
-    telephone: 'telephone2',
-    customer_type: 'customer',
-    email: 'hello1@computer.com',
-  },
-]
-
-const columns: ColumnType[] = [
-  {
-    key: 'name',
-    name: 'Name',
-    render: (value, record) => {
-      return (
-        <Box>
-          <Box display="flex" gap="5px">
-            <Chip
-              label={value.slice(0, 2)}
-              size="small"
-              sx={{ borderRadius: '2px' }}
-            />
-            <Box display="flex" flexDirection="column">
-              <b>{value}</b>
-              <span>{record.email}</span>
-            </Box>
-          </Box>
-        </Box>
-      )
-    },
-  },
-  { key: 'address', name: 'Address' },
-  { key: 'telephone', name: 'Telephone' },
-  {
-    key: 'customer_type',
-    name: 'Customer Type',
-    render: (value) => (
-      <Chip
-        label={value}
-        color={value === 'person' ? 'primary' : 'success'}
-        size="small"
-        sx={{ borderRadius: '2px' }}
-      />
-    ),
-  },
-  {
-    key: 'id',
-    align: 'right',
-    render: (value) => (
-      <Button
-        variant="contained"
-        startIcon={<EditIcon />}
-        size="small"
-        component={RouterLink}
-        to={`/customers/${value}/edit`}
-      >
-        Edit
-      </Button>
-    ),
-  },
-]
+type SelectedUserType = User | null | undefined
 
 const InvoiceIndex = () => {
+  const { users } = useUsers()
+  const { deleteUserMutation } = useDeleteUser()
+
+  const [open, setOpen] = useState<boolean>(false)
+  const [selectedUser, setSelectedUser] = useState<SelectedUserType>(null)
+
+  const handleDeleteClick = (id: number) => {
+    setSelectedUser(users.find((item) => item.id === id))
+    setOpen(true)
+  }
+
+  const columns: ColumnType[] = useMemo(
+    () => [
+      {
+        key: 'display_name',
+        name: 'Name',
+        render: (value, record) => `${record.first_name} ${record.last_name}`,
+      },
+      { key: 'email', name: 'Email' },
+      { key: 'phone_number', name: 'Phone' },
+      {
+        key: 'role',
+        name: 'Role',
+        render: (role) => (
+          <Chip
+            label={role}
+            color="primary"
+            sx={{ borderRadius: '2px' }}
+            size="small"
+          />
+        ),
+      },
+      {
+        key: 'id',
+        align: 'right',
+        render: (value) => (
+          <Box display="flex" justifyContent="flex-end" sx={{ gap: '10px' }}>
+            <Button
+              variant="contained"
+              startIcon={<EditIcon />}
+              size="small"
+              component={RouterLink}
+              to={`/admin/users/${value}/edit`}
+              color="secondary"
+            >
+              Edit
+            </Button>
+            <Button
+              variant="contained"
+              startIcon={<DeleteIcon />}
+              size="small"
+              onClick={() => handleDeleteClick(value)}
+              color="error"
+            >
+              Delete
+            </Button>
+          </Box>
+        ),
+      },
+    ],
+    [handleDeleteClick]
+  )
+
   return (
     <Box>
       <Box display="flex" justifyContent="space-between" alignItems="center">
@@ -123,7 +121,15 @@ const InvoiceIndex = () => {
         </Link>
       </Alert>
 
-      <ProTable columns={columns} data={mockData} />
+      <ProTable columns={columns} data={users} />
+
+      <ConfirmDialog
+        open={open}
+        setOpen={setOpen}
+        title={`Delete ${selectedUser?.first_name} ${selectedUser?.last_name}`}
+        content="Are you sure you want to delete this user?"
+        onSubmit={() => deleteUserMutation(selectedUser?.id)}
+      />
     </Box>
   )
 }
