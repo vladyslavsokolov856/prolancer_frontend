4a356b63b6fd7bef14d8d56469cc8602c34893a5
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-31 03:40:12 +0800

    save pdf layout and styles

--- src/components/Pdf/TaskWorkLogPdf.tsx
+++ src/components/Pdf/TaskWorkLogPdf.tsx
@@ -2,24 +2,112 @@ import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer'
 
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4',
+    padding: 20,
+    fontSize: 10,
   },
   section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
+    marginBottom: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // marginBottom: 10,
+  },
+  col: {
+    flex: 1,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  table: {
+    width: '100%',
+    // borderTop: '1px solid #ccc',
+    // borderBottom: '1px solid #ccc',
+  },
+  tableHeader: {
+    borderBottom: '1px solid #ccc',
+  },
+  tableRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // borderBottom: '1px solid #ccc',
+  },
+  tableCell: {
+    flex: 1,
+    padding: 5,
+    border: '1px solid #000',
+  },
+  tableCellFirst: {
+    borderLeft: 'none',
+  },
+  flex2: {
+    flex: 2,
+  },
+  flex3: {
+    flex: 3,
   },
 })
 
 export const TaskWorkLogPdf = () => (
   <Document>
-    <Page size="A4" style={styles.page}>
+    <Page style={styles.page}>
       <View style={styles.section}>
-        <Text>Section #1</Text>
+        <View style={styles.row}>
+          <Text style={styles.col}>Time registration</Text>
+          <Text style={[styles.col, { textAlign: 'right' }]}>factofly</Text>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.col}>
+            <Text style={styles.bold}>User</Text>
+            <Text>User Name</Text>
+          </View>
+          <View style={styles.col}>
+            <Text style={styles.bold}>Customer</Text>
+            <Text>Customer Name</Text>
+            <Text>Customer Address</Text>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <View style={styles.col}>
+            <Text style={styles.bold}>Task Information</Text>
+            <View style={styles.row}>
+              <Text style={styles.col}>Start date</Text>
+              <Text style={[styles.col, { textAlign: 'right' }]}>value</Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.col}>End date</Text>
+              <Text style={[styles.col, { textAlign: 'right' }]}>value</Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.col}>Number of hours</Text>
+              <Text style={[styles.col, { textAlign: 'right' }]}>value</Text>
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.col}>Job type</Text>
+              <Text style={[styles.col, { textAlign: 'right' }]}>value</Text>
+            </View>
+          </View>
+          <View style={styles.col}>
+            <Text style={styles.bold}>Title</Text>
+            <Text>Title Name</Text>
+          </View>
+        </View>
       </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
+      <View style={styles.table}>
+        <View style={[styles.tableRow, styles.tableHeader]}>
+          <Text style={[styles.tableCell, styles.flex2]}>Start date</Text>
+          <Text style={[styles.tableCell, styles.flex2]}>Start time</Text>
+          <Text style={[styles.tableCell, styles.flex2]}>
+            Duration (minutes)
+          </Text>
+          <Text style={[styles.tableCell, styles.flex3]}>Notes</Text>
+        </View>
+        <View style={styles.tableRow}>
+          <Text style={[styles.tableCell, styles.flex2]}>value</Text>
+          <Text style={[styles.tableCell, styles.flex2]}>value</Text>
+          <Text style={[styles.tableCell, styles.flex2]}>value</Text>
+          <Text style={[styles.tableCell, styles.flex3]}>Notes</Text>
+        </View>
       </View>
     </Page>
   </Document>
