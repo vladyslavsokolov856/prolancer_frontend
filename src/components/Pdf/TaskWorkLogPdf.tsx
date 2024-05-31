61bc12297ff6709becbdfe707c7289ba3c18b809
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-05-31 14:41:44 +0800

    apply auth user name placeholder

--- src/components/Pdf/TaskWorkLogPdf.tsx
+++ src/components/Pdf/TaskWorkLogPdf.tsx
@@ -51,67 +51,70 @@ const styles = StyleSheet.create({
   },
 })
 
-export const TaskWorkLogPdf = () => (
-  <Document>
-    <Page style={styles.page}>
-      <View style={styles.section}>
-        <View style={styles.row}>
-          <Text style={styles.col}>Time registration</Text>
-          <Text style={[styles.col, styles.textAlignRight]}>factofly</Text>
-        </View>
-        <View style={styles.row}>
-          <View style={styles.col}>
-            <Text style={styles.bold}>User</Text>
-            <Text>User Name</Text>
+export const TaskWorkLogPdf = () => {
+  const authUserName = 'John Doe'
+  return (
+    <Document>
+      <Page style={styles.page}>
+        <View style={styles.section}>
+          <View style={styles.row}>
+            <Text style={styles.col}>Time registration</Text>
+            <Text style={[styles.col, styles.textAlignRight]}>factofly</Text>
           </View>
-          <View style={styles.col}>
-            <Text style={styles.bold}>Customer</Text>
-            <Text>Customer Name</Text>
-            <Text>Customer Address</Text>
-          </View>
-        </View>
-        <View style={styles.row}>
-          <View style={styles.col}>
-            <Text style={styles.bold}>Task Information</Text>
-            <View style={styles.row}>
-              <Text style={styles.col}>Start date</Text>
-              <Text style={[styles.col, styles.textAlignRight]}>value</Text>
+          <View style={styles.row}>
+            <View style={styles.col}>
+              <Text style={styles.bold}>User</Text>
+              <Text>{authUserName}</Text>
             </View>
-            <View style={styles.row}>
-              <Text style={styles.col}>End date</Text>
-              <Text style={[styles.col, styles.textAlignRight]}>value</Text>
-            </View>
-            <View style={styles.row}>
-              <Text style={styles.col}>Number of hours</Text>
-              <Text style={[styles.col, styles.textAlignRight]}>value</Text>
-            </View>
-            <View style={styles.row}>
-              <Text style={styles.col}>Job type</Text>
-              <Text style={[styles.col, styles.textAlignRight]}>value</Text>
+            <View style={styles.col}>
+              <Text style={styles.bold}>Customer</Text>
+              <Text>Customer Name</Text>
+              <Text>Customer Address</Text>
             </View>
           </View>
-          <View style={styles.col}>
-            <Text style={styles.bold}>Title</Text>
-            <Text>Title Name</Text>
+          <View style={styles.row}>
+            <View style={styles.col}>
+              <Text style={styles.bold}>Task Information</Text>
+              <View style={styles.row}>
+                <Text style={styles.col}>Start date</Text>
+                <Text style={[styles.col, styles.textAlignRight]}>value</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.col}>End date</Text>
+                <Text style={[styles.col, styles.textAlignRight]}>value</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.col}>Number of hours</Text>
+                <Text style={[styles.col, styles.textAlignRight]}>value</Text>
+              </View>
+              <View style={styles.row}>
+                <Text style={styles.col}>Job type</Text>
+                <Text style={[styles.col, styles.textAlignRight]}>value</Text>
+              </View>
+            </View>
+            <View style={styles.col}>
+              <Text style={styles.bold}>Title</Text>
+              <Text>Title Name</Text>
+            </View>
           </View>
         </View>
-      </View>
-      <View style={styles.table}>
-        <View style={[styles.tableRow, styles.tableHeader]}>
-          <Text style={[styles.tableCell, styles.flex2]}>Start date</Text>
-          <Text style={[styles.tableCell, styles.flex2]}>Start time</Text>
-          <Text style={[styles.tableCell, styles.flex2]}>
-            Duration (minutes)
-          </Text>
-          <Text style={[styles.tableCell, styles.flex3]}>Notes</Text>
+        <View style={styles.table}>
+          <View style={[styles.tableRow, styles.tableHeader]}>
+            <Text style={[styles.tableCell, styles.flex2]}>Start date</Text>
+            <Text style={[styles.tableCell, styles.flex2]}>Start time</Text>
+            <Text style={[styles.tableCell, styles.flex2]}>
+              Duration (minutes)
+            </Text>
+            <Text style={[styles.tableCell, styles.flex3]}>Notes</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={[styles.tableCell, styles.flex2]}>value</Text>
+            <Text style={[styles.tableCell, styles.flex2]}>value</Text>
+            <Text style={[styles.tableCell, styles.flex2]}>value</Text>
+            <Text style={[styles.tableCell, styles.flex3]}>Notes</Text>
+          </View>
         </View>
-        <View style={styles.tableRow}>
-          <Text style={[styles.tableCell, styles.flex2]}>value</Text>
-          <Text style={[styles.tableCell, styles.flex2]}>value</Text>
-          <Text style={[styles.tableCell, styles.flex2]}>value</Text>
-          <Text style={[styles.tableCell, styles.flex3]}>Notes</Text>
-        </View>
-      </View>
-    </Page>
-  </Document>
-)
+      </Page>
+    </Document>
+  )
+}
