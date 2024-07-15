ca15aa27f8aea01f3f249cea8621c3a3a7bae774
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-07-15 05:22:21 -0500

    task35/integrate zitadel authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,30 @@ import '@/App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Layout from '@/components/Layout'
-import SignIn from '@/pages/signin'
-import SignUp from '@/pages/signup'
+// import SignIn from '@/pages/signin'
 import TaskDetailsByIdentifierPage from './pages/tasks/details'
+import { useContext } from 'react'
+import Login from './pages/signin_new'
+import Callback from './pages/callback'
+import { AuthContext, AuthContextType } from './context/auth'
 
 function App() {
+  const { token } = useContext(AuthContext) as AuthContextType
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signin" Component={SignIn} />
-        <Route path="/signup" Component={SignUp} />
+        {!token ? (
+          <>
+            <Route path="/signin" element={<Login />} />
+            <Route path="/callback" element={<Callback />} />
+          </>
+        ) : (
+          <Route path="*" Component={Layout} />
+        )}
         <Route
           path="/tasks/details/:identifier"
           Component={TaskDetailsByIdentifierPage}
         />
-        <Route path="*" Component={Layout} />
       </Routes>
     </BrowserRouter>
   )
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,11 +1,17 @@
 import axios, { AxiosInstance } from 'axios'
 
 const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || ''
+const ZITADEL_AUTHORITY = 'http://prolancer-authentication-ebcf8a4aae1e.herokuapp.com/'
+const ZITADEL_CLIENT_ID = '275389987892098434@prolancer'
+
+const userInfo = localStorage.getItem(`oidc.user:${ZITADEL_AUTHORITY}:${ZITADEL_CLIENT_ID}`) as string
+const token = JSON.parse(userInfo).access_token;
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: VITE_BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
+    "Authorization": `Bearer ${token}`
   },
 })
 
