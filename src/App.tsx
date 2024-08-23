f5ad4329bc41cc2b8788d8c49b4fdfa60de3bde1
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-08-23 07:21:45 -0500

    remove zitadel authentication

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,24 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from '@/components/Layout'
 // import SignIn from '@/pages/signin'
 import TaskDetailsByIdentifierPage from './pages/tasks/details'
-import { useContext } from 'react'
-import Login from './pages/signin_new'
-import Callback from './pages/callback'
-import { AuthContext, AuthContextType } from './context/auth'
+// import { useContext } from 'react'
+// import Login from './pages/signin_new'
+// import Callback from './pages/callback'
+// import { AuthContext, AuthContextType } from './context/auth'
 
 function App() {
-  const { token } = useContext(AuthContext) as AuthContextType
+  // const { token } = useContext(AuthContext) as AuthContextType
   return (
     <BrowserRouter>
       <Routes>
-        {!token ? (
+        {/* {!token ? (
           <>
             <Route path="/signin" element={<Login />} />
             <Route path="/callback" element={<Callback />} />
           </>
-        ) : (
+        ) : ( */}
           <Route path="*" Component={Layout} />
-        )}
+        {/* )} */}
         <Route
           path="/tasks/details/:identifier"
           Component={TaskDetailsByIdentifierPage}
--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { SnackbarProvider } from 'notistack'
-import { AuthProvider } from './context/auth.tsx'
+// import { AuthProvider } from './context/auth.tsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
+    {/* <AuthProvider> */}
       <ThemeProvider theme={light}>
         <SnackbarProvider maxSnack={3}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -26,6 +26,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           </LocalizationProvider>
         </SnackbarProvider>
       </ThemeProvider>
-    </AuthProvider>
+    {/* </AuthProvider> */}
   </React.StrictMode>
 )
