f208eb5ce89386718772208f492488bf3c89ac04
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-08-29 11:58:27 -0700

    Replace all text loading bars with Material UI circular progress spinners

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,15 +1,18 @@
 import { useContext } from 'react'
 import { AuthContext, AuthContextType } from '@/context/auth'
-import { Navigate } from "react-router";
+import { Navigate } from 'react-router'
+import { CircularProgress } from '@mui/material'
 
 const Signin = () => {
   const { signin, authenticated } = useContext(AuthContext) as AuthContextType
 
-  return <>
-    {authenticated === null && <div>Login Loading...</div>}
-    {authenticated === false && signin()}
-    {authenticated && <Navigate to="/" />}
-  </>
+  return (
+    <>
+      {authenticated === null && <CircularProgress />}
+      {authenticated === false && signin()}
+      {authenticated && <Navigate to="/" />}
+    </>
+  )
 }
 
 export default Signin
