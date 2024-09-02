a48b28743386ed2c444245b8cd4e2e5f360c70ac
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-09-02 16:00:33 +0200

    axios configuration

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,8 +3,9 @@ import axios from 'axios'
 const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || ''
 
 export const configAxios = (userInfo: any) => {
-  const { access_token } = userInfo || {};
+  const { id_token } = userInfo || {};
 
   axios.defaults.baseURL = VITE_BACKEND_URL;
-  axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
+  // axios.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
+  axios.defaults.headers.common['X-ID'] = id_token;
 };
