bc4f4b72cd5c94181eab7b5266149577d7511100
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-09-03 09:09:00 +0800

    rename response to data

--- src/services/currencyRateService.ts
+++ src/services/currencyRateService.ts
@@ -3,7 +3,7 @@ const API_URL = 'https://api.exchangerate-api.com/v4/latest/DKK'
 export const fetchCurrencyRates = async () => {
   return fetch(API_URL)
     .then((response) => response.json())
-    .then((response) => {
-      return response?.rates
+    .then((data) => {
+      return data?.rates
     })
 }
