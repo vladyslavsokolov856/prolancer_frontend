ed3267f68a16318c3efcf4b2946f25e4dfb621c3
Vladyslav Sokolov <sokolovvladyslav856@gmail.com>
2024-09-03 08:56:26 +0800

    replace axios with fetch
    in order to fetch without the default headers that i believe cause the cors issue

--- src/services/currencyRateService.ts
+++ src/services/currencyRateService.ts
@@ -3,6 +3,9 @@ import axios from 'axios'
 const API_URL = 'https://api.exchangerate-api.com/v4/latest/DKK'
 
 export const fetchCurrencyRates = async () => {
-  const response = await axios.get(API_URL)
-  return response?.data?.rates
+  return fetch(API_URL)
+    .then((response) => response.json())
+    .then((response) => {
+      return response?.rates
+    })
 }
